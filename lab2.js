f1c9e70540c7eba19f60b3e190ea16c27c73478d
JikanS5 <charli.deluca@yahoo.fr>
2020-03-12 16:11:24 +0000

    finis

---
+++ lab2.js
@@ -0,0 +1,19 @@
+function myAlphabetLength(array)
+{
+    console.log(array.length)
+}
+var Planets= ['Earth','Mars','Saturn','Jupiter','Neptune']
+for (let index = 0; index < Planets.length; index++) {
+    console.log(Planets[index])
+}
+
+var wowDatatypes=['Yes',42,false]
+for (let index = 0; index < wowDatatypes.length; index++) {
+    console.log(wowDatatypes[index])
+    
+}
+var hello = function(){
+    return 'Hello world!'
+}
+console.log(hello)
+
